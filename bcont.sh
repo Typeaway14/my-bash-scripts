a7b50e0edb10a20411938922466911f492a79179
Typeaway14 <harsh.som33@gmail.com>
2022-09-05 14:54:28 +0530

    documentation update #1

--- bcont.sh
+++ bcont.sh
@@ -3,10 +3,13 @@
 #author: Typeaway14
 #purpose: control display brightness more precisely than keyboard hotkey
 
+#use xrandr to find display 'name'. eDP on my laptop
+
 VALUE=${1:-1}
 
 if [[ $VALUE > 0.19999 && $VALUE < 1.00001 ]];
 then
+	#xrandr --output 'name' --brightness $VALUE
 	xrandr --output eDP --brightness $VALUE
 else
 	echo "Please provide value >0.2 and lesser than 1"
